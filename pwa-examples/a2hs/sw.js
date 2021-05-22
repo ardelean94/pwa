8012dde172e7c48c55d4f9643fcab99b6d594f3c
Alex <ardeleanalexdaniel@yahoo.com>
2021-05-22 13:49:47 +0300

    Replace double quotes with single qoutes

--- pwa-examples/a2hs/sw.js
+++ pwa-examples/a2hs/sw.js
@@ -1,26 +1,23 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches
-      .open("fox-store")
-      .then((cache) =>
-        cache.addAll([
-          "/pwa/pwa-examples/a2hs/",
-          "/pwa/pwa-examples/a2hs/index.html",
-          "/pwa/pwa-examples/a2hs/index.js",
-          "/pwa/pwa-examples/a2hs/style.css",
-          "/pwa/pwa-examples/a2hs/images/fox1.jpg",
-          "/pwa/pwa-examples/a2hs/images/fox2.jpg",
-          "/pwa/pwa-examples/a2hs/images/fox3.jpg",
-          "/pwa/pwa-examples/a2hs/images/fox4.jpg",
-        ])
-      )
-  );
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('fox-store').then(function(cache) {
+     return cache.addAll([
+       '/pwa/pwa-examples/a2hs/',
+       '/pwa/pwa-examples/a2hs/index.html',
+       '/pwa/pwa-examples/a2hs/index.js',
+       '/pwa/pwa-examples/a2hs/style.css',
+       '/pwa/pwa-examples/a2hs/images/fox1.jpg',
+       '/pwa/pwa-examples/a2hs/images/fox2.jpg',
+       '/pwa/pwa-examples/a2hs/images/fox3.jpg',
+       '/pwa/pwa-examples/a2hs/images/fox4.jpg'
+     ]);
+   })
+ );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', function(e) {
   console.log(e.request.url);
   e.respondWith(
-    //caches.match(e.request).then((response) => response || fetch(e.request))
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
